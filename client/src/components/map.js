969e1b2b4c308868fe2daf0a6a00d754857b26cf
PetrusAriaa <petrusariacr25@gmail.com>
2022-11-24 03:19:14 +0700

    first commit

---
+++ client/src/components/map.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet/dist/leaflet';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import 'leaflet-draw/dist/leaflet.draw';
+function Map(){
+
+    useEffect(()=>{
+        const GSP_coord = [-7.770121424862446,110.37784742786181];
+
+    })
+
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Map;
